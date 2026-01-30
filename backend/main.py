b6f64f80f7cb9b8287c3f00121fcf409d3b4d51f
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2026-01-30 22:35:24 +0500

    start working on backend, create an endpoint for word to pdf conversion, still implementing it

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import UploadFile
+from spire.doc import Document
+from spire.doc import FileFormat
+
+app = FastAPI()
+
+@app.post("/word_to_pdf")
+def word_to_pdf(file: UploadFile):
+    name, ext = file.filename.split(".")
+    document = Document()
+    print(file.filename)
+    document.LoadFromFile(file.filename)
+    document.SaveToFile(name + ".pdf", fileFormat=FileFormat.PDF)
+    document.Close()
+
+    # return the resultant file
+
+    
